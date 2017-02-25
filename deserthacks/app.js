8759db816dc2b30fb9c4d33a29b71d2c38bff2f6
James Quigley <james@quigley.us>
2017-02-25 16:27:42 -0700

    starting API

--- deserthacks/app.js
+++ deserthacks/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var passport = require('passport');
 var index = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -58,6 +59,7 @@ app.use(passport.initialize());
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
