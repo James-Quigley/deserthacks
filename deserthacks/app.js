b2e22db3cec16d09ca75f3b459b4982ae6027684
James Quigley <james@quigley.us>
2017-02-25 15:30:59 -0700

    Added passport-slack

--- deserthacks/app.js
+++ deserthacks/app.js
@@ -10,6 +10,29 @@ var users = require('./routes/users');
 
 var app = express();
 
+const {
+    CLIENT_ID,
+    CLIENT_SECRET,
+    PORT
+} = process.env,
+    SlackStrategy = require('passport-slack').Strategy,
+    passport = require('passport'),
+    express = require('express'),
+    app = express();
+
+// setup the strategy using defaults
+passport.use(new SlackStrategy({
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET
+}, (accessToken, refreshToken, profile, done) => {
+    // optionally persist profile data
+    done(null, profile);
+}));
+app.use(passport.initialize());
+app.use(require('body-parser').urlencoded({
+    extended: true
+}));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,7 +41,6 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,20 +49,31 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
+
+// path to start the OAuth flow
+app.get('/auth/slack', passport.authorize('slack'));
+
+// OAuth callback url
+app.get('/auth/slack/callback',
+    passport.authorize('slack', {
+        failureRedirect: '/login'
+    }),
+    (req, res) => res.redirect('/')
+);
 
 module.exports = app;
