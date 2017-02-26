9999529b1ebeabe5257c75ec9bf50e04acd36c47
James Quigley <james@quigley.us>
2017-02-26 11:11:51 -0700

    Added AWS Lambda Function code

---
+++ VirtualKeyality/src/npm-scripts/deploy-s3-cors.js
@@ -0,0 +1,17 @@
+const npmRunScript = require('npm-run-script');
+const argv = require('attrs.argv');
+
+const stage = argv.stage;
+const configPath = '../config/' + stage + '.skill.config.json';
+const config = require(configPath);
+const helper = require('./helper');
+
+const bucketName = config.s3.bucketName;
+const skillNamespace = config.skillNamespace;
+const profileName = helper.getProfileName(skillNamespace, stage);
+
+const command = 'aws s3api put-bucket-cors --bucket ' + bucketName + ' --cors-configuration file://s3-cors.json --profile ' + profileName;
+
+const child = npmRunScript(command, { stdio: 'inherit' }); 
+child.once('error', (error) => { process.exit(1); });
+child.once('exit', (exitCode) => { process.exit(exitCode); });
