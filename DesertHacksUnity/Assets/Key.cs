5299236b33b07ec1f6a8161a9ef6d4a11e1ae00f
abahrema <abahrema@asu.edu>
2017-02-26 11:28:30 -0700

    Unity Implementation
    Unity Package with Keyboard and Solar System.

---
+++ DesertHacksUnity/Assets/Key.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+	Transform tr;
+	Rigidbody rb;
+	public char listenKey;
+
+	//public Transform CameraRig;              // Variable to reference position, rotation, and scale of camera rig object, we will use this for grabbing position to spawn objects.
+	//public GameObject keyObject;
+    public Rigidbody keyObject;
+    public int rotationTransform = 0;
+
+	//public float MoveSpeed = 1;
+	public float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = new Vector3 (0.0f, 0.0f, -3.0f);
+	}
+	
+	// Update is called once per fixed period of time
+	void Update () {
+//		if (Input.GetKey (listenKey.ToString()))
+//			Debug.Log("Triggered this key object: " + listenKey);
+
+	}
+	void spawn(){
+	}
+	public void setListenKey(char letter){
+		listenKey = letter;
+	}
+}
---
+++ DesertHacksUnity/Assets/MonoStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoStart : MonoBehaviour {
+	public GameController gc;
+	// Use this for initialization
+	void Start () {
+		gc = new GameController();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
